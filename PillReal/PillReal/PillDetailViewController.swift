b932f4ee35b595d67f9ec7972b52f1bf4769e77d
wndzlf <wndzlf@gmail.com>
2018-01-26 17:42:02 +0900

    pillreal에 있어요

---
+++ PillReal/PillReal/PillDetailViewController.swift
@@ -0,0 +1,39 @@
+//
+//  PillDetailViewController.swift
+//  PillReal
+//
+//  Created by admin on 2018. 1. 26..
+//  Copyright © 2018년 wndzlf. All rights reserved.
+//
+
+import UIKit
+
+class PillDetailViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+
+        return cell
+    }
+}
+
---
+++ PillReal/PillReal/PillTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  PillTableViewCell.swift
+//  PillReal
+//
+//  Created by admin on 2018. 1. 26..
+//  Copyright © 2018년 wndzlf. All rights reserved.
+//
+
+import UIKit
+
+class PillTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var pillimage : UIImageView!
+    @IBOutlet weak var label : UILabel!
+}
